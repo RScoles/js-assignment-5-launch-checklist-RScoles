260f76b490d65803a4816748b683496f984fd313
RScoles <ryanscoles@ryans-Air.attlocal.net>
2023-03-10 17:54:37 -0600

    Down to 2 failed tests

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 window.addEventListener("load", function() {
     let list = document.getElementById("faultyItems");
+    list.style.visibility = "hidden";
     let form = document.querySelector("form");
     
 form.addEventListener("submit", function(event) {
